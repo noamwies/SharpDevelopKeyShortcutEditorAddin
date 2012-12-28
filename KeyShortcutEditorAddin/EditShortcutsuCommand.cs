af9c422a53f614930c3f64e487a772167532d878
Noam Wies <noamwies@gmail.com>
2012-12-28 13:18:35 +0200

    set addin configuration

---
+++ KeyShortcutEditorAddin/EditShortcutsuCommand.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Noam
+ * Date: 28/12/2012
+ * Time: 13:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using ICSharpCode.Core;
+
+namespace KeyShortcutEditorAddin
+{
+	/// <summary>
+	/// Description of EditShortcutsuCommand.
+	/// </summary>
+	public class EditShortcutsuCommand : AbstractMenuCommand
+	{
+		public EditShortcutsuCommand()
+		{
+			
+		}
+		
+		public override void Run(){
+				
+		}
+	}
+}
