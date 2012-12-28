8f77052bd6869f89ad3f14465d89e8e1859aa277
Noam Wies <noamwies@gmail.com>
2012-12-28 14:48:04 +0200

    change addin to use option panel

---
+++ KeyShortcutEditorAddin/EditShortcutsuPanel.xaml.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Noam
+ * Date: 28/12/2012
+ * Time: 14:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace KeyShortcutEditorAddin
+{
+	/// <summary>
+	/// Interaction logic for EditShortcutsuPanel.xaml
+	/// </summary>
+	public partial class EditShortcutsuPanel : OptionPanel
+	{
+		public EditShortcutsuPanel()
+		{
+			InitializeComponent();
+		}
+	}
+}
