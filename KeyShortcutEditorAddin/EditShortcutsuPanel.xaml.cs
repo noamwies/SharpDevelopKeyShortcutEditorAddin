af0a9714803ea6d67f4f959a5363c91a13fbc32d
Noam Wies <noamwies@gmail.com>
2012-12-30 12:16:43 +0200

    add default directory for shortcuts template

--- KeyShortcutEditorAddin/EditShortcutsuPanel.xaml.cs
+++ KeyShortcutEditorAddin/EditShortcutsuPanel.xaml.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,6 +33,11 @@ namespace KeyShortcutEditorAddin
 		private EditKeyShortcutInAddinFiles _shortcutsEditor;
 		private XmlSerializer _serialzer;
 		private static readonly string SHARP_DEVELOP_SHORTCUTS_FILTER = "#Develop Shortcuts|*.sht";
+		private static readonly string SHORTCUTS_DEFAULT_DIRECTORY;
+		
+		static EditShortcutsuPanel(){
+			SHORTCUTS_DEFAULT_DIRECTORY = Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof(EditShortcutsuPanel)).Location),"Shortcuts");
+		}
 		
 		public EditShortcutsuPanel()
 		{
@@ -44,9 +50,13 @@ namespace KeyShortcutEditorAddin
 		{
 			var dialog = new SaveFileDialog();
 			dialog.Filter = SHARP_DEVELOP_SHORTCUTS_FILTER;
+			dialog.InitialDirectory = SHORTCUTS_DEFAULT_DIRECTORY;
 			var result = dialog.ShowDialog();
 			if (result == DialogResult.OK) {
 				string path = dialog.FileName;
+				if (dialog.CheckPathExists) {
+					Directory.CreateDirectory(path);
+				}
 				if (false == String.IsNullOrEmpty(path)){
 					using (var file = new FileStream(path,FileMode.OpenOrCreate,FileAccess.Write))
 					{
@@ -62,6 +72,7 @@ namespace KeyShortcutEditorAddin
 			var dialog = new OpenFileDialog();
 			var result = dialog.ShowDialog();
 			dialog.Filter = SHARP_DEVELOP_SHORTCUTS_FILTER;
+			dialog.InitialDirectory = SHORTCUTS_DEFAULT_DIRECTORY;
 			if (result == DialogResult.OK) {
 				string path = dialog.FileName;
 				if (false == String.IsNullOrEmpty(path)){
