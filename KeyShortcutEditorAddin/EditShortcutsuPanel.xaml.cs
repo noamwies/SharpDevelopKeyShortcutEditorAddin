a829d524b2423208343c070841a0197e6b67fbac
Noam Wies <noamwies@gmail.com>
2012-12-30 10:43:48 +0200

    fix buil and add basic implemention of export and import shortcuts:

--- KeyShortcutEditorAddin/EditShortcutsuPanel.xaml.cs
+++ KeyShortcutEditorAddin/EditShortcutsuPanel.xaml.cs
@@ -8,13 +8,16 @@
  */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Serialization;
 using ICSharpCode.SharpDevelop.Gui;
 
 namespace KeyShortcutEditorAddin
@@ -24,20 +27,35 @@ namespace KeyShortcutEditorAddin
 	/// </summary>
 	public partial class EditShortcutsuPanel : OptionPanel
 	{
+		private EditKeyShortcutInAddinFiles _shortcutsEditor;
+		private XmlSerializer _serialzer;
+		
 		public EditShortcutsuPanel()
 		{
 			InitializeComponent();
+			_serialzer = new XmlSerializer(typeof(List<KeyShortcut>));
+			_shortcutsEditor = new EditKeyShortcutInAddinFiles();
 		}
 		
 		public void Export(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("Export");
-		}
-		
+			string path = "";
+			using (var file = new FileStream(path,FileMode.CreateNew))
+			{
+				_serialzer.Serialize(file,_shortcutsEditor.KeyShortcuts);				
+			}
+		}		
+
 		public void Import(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("Import");
-		}
-		
+			string path = "";
+			using (var file = new FileStream(path,FileMode.Open))
+			{
+				var shortcuts = _serialzer.Deserialize(file) as List<KeyShortcut>;	
+				foreach (var s in shortcuts) {
+					_shortcutsEditor.ChangeKeyShortcut(s.Label,s.Shortcut);
+				}				
+			}
+		}		
 	}
 }
